038367735dc3502f0eea99773e5f291015bcfe25
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-11-22 10:45:12 +0000

    Created Exercises001 class

---
+++ Functional-programming/Functional-programming/Exercises001.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_programming
+{
+    internal class Exercises001
+    {
+        static Func<int, int> SquareIt = x => x * x;
+    }
+}
