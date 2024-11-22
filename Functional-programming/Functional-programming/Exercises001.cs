3d60060ffc650505e88df49d6d5e0d3ecbd1fbb5
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-11-22 11:09:43 +0000

    Implement Grammar check

--- Functional-programming/Functional-programming/Exercises001.cs
+++ Functional-programming/Functional-programming/Exercises001.cs
@@ -7,15 +7,11 @@ using System.Threading.Tasks;
 
 namespace Functional_programming
 {
-    public class Exercises001
+    public static class Exercises001
     {
         public static Func<int, int> SquareIt = x => x * x;
         public static Func<int, int> AddTen = x => x + 10;
+        public static Predicate<string> GrammarCheck = s => s[0] == 'A' && s[s.Length - 1] == '!';
         public static List<int> Numbers = new List<int> { 4, 15, 55, 78, 12 };
-        public void Run()
-        {
-            List<int> results = Numbers.Select(AddTen).ToList();
-            results.ForEach(x => Console.WriteLine(x));
-        }
     }
 }
--- Functional-programming/Functional-programming/Program.cs
+++ Functional-programming/Functional-programming/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Exercises001 ex = new();
-            ex.Run();
+            List<int> results = Exercises001.Numbers.Select(Exercises001.AddTen).ToList();
+            results.ForEach(x => Console.WriteLine(x));
+            List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
+            words.ForEach(x => Console.WriteLine(Exercises001.GrammarCheck(x)));
         }
     }
 }
